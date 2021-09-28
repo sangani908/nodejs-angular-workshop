c9673391f6b414681ec0d3cdb9b999681aa81293
sangani908 <91524276+sangani908@users.noreply.github.com>
2021-09-28 12:49:25 +0530

    Day1
    Workshop Node Js

---
+++ simple.js
@@ -0,0 +1,15 @@
+
+var a=10;
+var b=30;
+var c=a+b;
+console.log("A value is "+a);
+console.log("B value is "+b);
+
+if(c > 30)
+{
+    console.log("c is > 30");
+}
+else{
+    console.log("c is < 30");
+}
+
